bad99952aaef9d0ca402adf7cd571939ca2c8b2a
levi <levidousseaux@gmail.com>
2021-01-14 21:26:48 -0300

    user controller test

--- dist/controller/UserController.js
+++ dist/controller/UserController.js
@@ -6,8 +6,8 @@ class UserController {
     GetAll(req, res) {
         try {
             //PEDIR PARA O BANCO RETORNAR O USUARIO DO ID
-            const response = new User_1.User("Levi", "levi1234", 1); //userRepository.find(req.params.id)      
-            res.status(200).send(response);
+            const user = new User_1.User(req.body.name, req.body.email, req.body.password);
+            res.status(200).send(user);
         }
         catch (e) {
             res.status(404).send(e.message);
